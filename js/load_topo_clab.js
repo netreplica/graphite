ee8a4f01cfb0687cfdfd47fffe1869a451158da8
Alex Bortok <dev@bortok.com>
2022-02-09 15:14:22 -0800

    layerSortPreference from nodes[].labels.group-level

--- js/load_topo_clab.js
+++ js/load_topo_clab.js
@@ -9,7 +9,8 @@ var clab_graph_json = {
       "ipv4_address": "172.20.20.3/24",
       "ipv6_address": "2001:172:20:20::3/64",
       "labels": {
-        "graph-icon": "server"
+        "graph-icon": "server",
+        "graph-level": 3,
       },
     },
     {
@@ -20,7 +21,8 @@ var clab_graph_json = {
       "ipv4_address": "172.20.20.5/24",
       "ipv6_address": "2001:172:20:20::5/64",
       "labels": {
-        "graph-icon": "server"
+        "graph-icon": "server",
+        "graph-level": 3,
       },
     },
     {
@@ -32,7 +34,8 @@ var clab_graph_json = {
       "ipv6_address": "2001:172:20:20::2/64",
       "labels": {
         "graph-icon": "switch",
-        "graph-something": "whatever"
+        "graph-level": 2,
+        "graph-something": "whatever",
       },
     },
     {
@@ -43,7 +46,8 @@ var clab_graph_json = {
       "ipv4_address": "172.20.20.6/24",
       "ipv6_address": "2001:172:20:20::6/64",
       "labels": {
-        "graph-icon": "switch"
+        "graph-icon": "switch",
+        "graph-level": 2,
       },
     },
     {
@@ -54,7 +58,8 @@ var clab_graph_json = {
       "ipv4_address": "172.20.20.4/24",
       "ipv6_address": "2001:172:20:20::4/64",
       "labels": {
-        "graph-icon": "router"
+        "graph-icon": "router",
+        "graph-level": 1,
       },
     },
   ],
@@ -93,30 +98,35 @@ function generate_cmt_from_clab_graph_json(c){
   for (var i =0; i < c.nodes.length; i++) {
     var n = c.nodes[i]
     var icon = "router"
+    var level
     node_id_map[n.name] = i
     if (n.hasOwnProperty("labels")) {
       if (n.labels.hasOwnProperty("graph-icon")) {
         icon = n.labels["graph-icon"]
       }
+      if (n.labels.hasOwnProperty("graph-level")) {
+        level = n.labels["graph-level"]
+      }
     }
     cmt.nodes.push({
-        "id": i,
-        "name": n.name,
-        "primaryIP": n.ipv4_address,
-        "model": n.kind,
-        "icon": icon,
+      "id": i,
+      "name": n.name,
+      "primaryIP": n.ipv4_address,
+      "model": n.kind,
+      "icon": icon,
+      "layerSortPreference": level,
     })
   }
   for (var i =0; i < c.links.length; i++) {
     var l = c.links[i]
     cmt.links.push({
-        "id": i,
-        "source": node_id_map[l["source"]],
-        "target": node_id_map[l["target"]],
-        "srcIfName": l["source_endpoint"],
-        "srcDevice": l["source"],
-        "tgtIfName": l["target_endpoint"],
-        "tgtDevice": l["target"],
+      "id": i,
+      "source": node_id_map[l["source"]],
+      "target": node_id_map[l["target"]],
+      "srcIfName": l["source_endpoint"],
+      "srcDevice": l["source"],
+      "tgtIfName": l["target_endpoint"],
+      "tgtDevice": l["target"],
     })
   }
   return cmt
