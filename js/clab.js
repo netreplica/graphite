72cf76431b27ac15782d8b384c541712df413df1
Alex Bortok <dev@bortok.com>
2022-02-09 21:24:00 -0800

    validate ipv4_address presence

--- js/clab.js
+++ js/clab.js
@@ -1,31 +1,35 @@
 // Convert ContainerLab Graph JSON export into CMT JSON topology
 function convert_clab_graph_to_cmt(c){
-  var cmt = {"nodes": [], "links": []}
-  var node_id_map = {}
+  var cmt = {"nodes": [], "links": []};
+  var node_id_map = {};
   for (var i =0; i < c.nodes.length; i++) {
-    var n = c.nodes[i]
-    var icon = "router"
-    var level
-    node_id_map[n.name] = i
+    var n = c.nodes[i];
+    var primaryIP;
+    var icon = "router";
+    var level;
+    node_id_map[n.name] = i;
+    if (n.hasOwnProperty("ipv4_address")) {
+      primaryIP = n.ipv4_address;
+    }
     if (n.hasOwnProperty("labels")) {
       if (n.labels.hasOwnProperty("graph-icon")) {
-        icon = n.labels["graph-icon"]
+        icon = n.labels["graph-icon"];
       }
       if (n.labels.hasOwnProperty("graph-level")) {
-        level = n.labels["graph-level"]
+        level = n.labels["graph-level"];
       }
     }
     cmt.nodes.push({
       "id": i,
       "name": n.name,
-      "primaryIP": n.ipv4_address,
       "model": n.kind,
+      "primaryIP": primaryIP,
       "icon": icon,
       "layerSortPreference": level,
     })
   }
   for (var i =0; i < c.links.length; i++) {
-    var l = c.links[i]
+    var l = c.links[i];
     cmt.links.push({
       "id": i,
       "source": node_id_map[l["source"]],
@@ -36,5 +40,5 @@ function convert_clab_graph_to_cmt(c){
       "tgtDevice": l["target"],
     })
   }
-  return cmt
+  return cmt;
 }
