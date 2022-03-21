67e116c049db0ed10ee0695a95a7468d6aaadaf0
Alex Bortok <dev@bortok.com>
2022-03-20 21:31:05 -0700

    generate graph for default topo

--- docker/graphite/generate_all_offline_graphs.sh
+++ docker/graphite/generate_all_offline_graphs.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # This script will generate offline graphs in JSON format for all Containerlab YAML topology files found in $WWW_HOME/clab directory
 
-cd $WWW_HOME/clab
+if [ -d $WWW_HOME/clab ]; then
+  cd $WWW_HOME/clab
+fi
 
 for topo in *.yaml
 do
--- docker/graphite/generate_offline_graph.sh
+++ docker/graphite/generate_offline_graph.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 # This script will generate offline graphs in JSON format from specified Containerlab YAML topology file
 
-cd $WWW_HOME/clab
+if [ -d $WWW_HOME/clab ]; then
+  cd $WWW_HOME/clab
+fi
 
-topo=$1
+if [ "$#" -lt 1 ]; then
+  if [ -n "$GRAPHITE_DEFAULT_TOPO" ]; then
+    topo="${GRAPHITE_DEFAULT_TOPO}.yaml"
+  else
+    echo Error: define topology name to visualize via GRAPHITE_DEFAULT_TOPO or as a parameter
+    exit 2
+  fi
+else
+  topo="$1"
+fi
 
-if [ -f $topo ] ; then
+if [ -f "$topo" ] ; then
+  echo Visualizing topology: "$topo"
   clabg graph --json --topo "$topo" --offline
 else
-  echo Error: no such file $topo
+  echo Error: no such file "$topo"
+  exit 2
 fi
