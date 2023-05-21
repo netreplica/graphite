90495414b161a82f6a9a64aa8e0eadb022c0fe21
Alex Bortok <dev@bortok.com>
2023-05-20 18:20:51 -0700

    indent

--- app/js/nextui.js
+++ app/js/nextui.js
@@ -901,184 +901,184 @@
        * @namespace nx.graphic.Topology
        */
       nx.define("GraphiteAlignedLabel", nx.graphic.Text, {
-          properties: {
-            text: {
-              /**
-               * Set/get label text
-               * @property text
-               * @type {String}
-               * @default ""
-               * @description Set/get label text
-               * @example
-               * <caption>Set the label text</caption>
-               * // Set the label text to "Hello"
-               * label.text("Hello");
-               */
-              get: function () {
-                  return this._text !== undefined ? this._text : "";
-              },
-              set: function (value) {
-                  if (value !== undefined && this._text !== value) {
-                      this._text = value;
-                      var el = this.view().dom().$dom;
-                      if (el.firstChild) {
-                          el.removeChild(el.firstChild);
-                      }
-                      this._textPath.textContent = this._text;
-                      el.appendChild(this._textPath);
-                      return true;
-                  } else {
-                      return false;
-                  }
-                }
+        properties: {
+          text: {
+            /**
+             * Set/get label text
+             * @property text
+             * @type {String}
+             * @default ""
+             * @description Set/get label text
+             * @example
+             * <caption>Set the label text</caption>
+             * // Set the label text to "Hello"
+             * label.text("Hello");
+             */
+            get: function () {
+                return this._text !== undefined ? this._text : "";
             },
-            side: {
-              /**
-               * Set/get on which side of a path the text should be rendered
-               * @property side
-               * @type {String}
-               * @default 'source'
-               * @description Possible values are 'source' and 'target'
-               * @example
-               * <caption>Set the side</caption>
-               * // Set the side to 'target'
-               * textPath.side('target');
-               */
-              get: function () {
-                  return this._side;
-              },
-              set: function (value) {
-                  if (value !== undefined && this._side !== value && (value === 'source' || value === 'target')) {
-                      this._side = value;
-                      if (this._side == 'source') {
-                        //this.offset(10);
-                        this.setStyle('text-anchor', 'start');
-                        this.setStyle('alignment-baseline', 'text-before-edge');
-                      } else {
-                        var pathLength = this._pathElement.getTotalLength();
-                        //this.offset(pathLength - 10);
-                        this.setStyle('text-anchor', 'end');
-                        this.setStyle('alignment-baseline', 'text-after-edge');
-                      }
-                      return true;
-                  } else {
-                      return false;
-                  }
+            set: function (value) {
+                if (value !== undefined && this._text !== value) {
+                    this._text = value;
+                    var el = this.view().dom().$dom;
+                    if (el.firstChild) {
+                        el.removeChild(el.firstChild);
+                    }
+                    this._textPath.textContent = this._text;
+                    el.appendChild(this._textPath);
+                    return true;
+                } else {
+                    return false;
+                }
               }
+          },
+          side: {
+            /**
+             * Set/get on which side of a path the text should be rendered
+             * @property side
+             * @type {String}
+             * @default 'source'
+             * @description Possible values are 'source' and 'target'
+             * @example
+             * <caption>Set the side</caption>
+             * // Set the side to 'target'
+             * textPath.side('target');
+             */
+            get: function () {
+                return this._side;
+            },
+            set: function (value) {
+                if (value !== undefined && this._side !== value && (value === 'source' || value === 'target')) {
+                    this._side = value;
+                    if (this._side == 'source') {
+                      //this.offset(10);
+                      this.setStyle('text-anchor', 'start');
+                      this.setStyle('alignment-baseline', 'text-before-edge');
+                    } else {
+                      var pathLength = this._pathElement.getTotalLength();
+                      //this.offset(pathLength - 10);
+                      this.setStyle('text-anchor', 'end');
+                      this.setStyle('alignment-baseline', 'text-after-edge');
+                    }
+                    return true;
+                } else {
+                    return false;
+                }
+            }
           },
           link: {
-              /**
-               * Set/get link instance with which the label is associated
-               * @property link
-               * @type {nx.graphic.Link}
-               * @default null
-               * @description Set/get link instance with which the label is associated
-               * @example
-               * <caption>Set the link instance with which the label is associated</caption>
-               * // Set the link instance to link1
-               * textPath.link(link1);
-               */
-              get: function () {
-                  return this._link !== undefined ? this._link : null;
-              },
-              set: function (value) {
-                  if (value !== undefined && this._link !== value && value !== null) {
-                      this._link = value;
-                      this._textPath.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#link_" + this._link.id());
-                      this._pathElement = this._link.view('path').dom().$dom;
-                      return true;
-                  } else {
-                      return false;
-                  }
-              }
+            /**
+             * Set/get link instance with which the label is associated
+             * @property link
+             * @type {nx.graphic.Link}
+             * @default null
+             * @description Set/get link instance with which the label is associated
+             * @example
+             * <caption>Set the link instance with which the label is associated</caption>
+             * // Set the link instance to link1
+             * textPath.link(link1);
+             */
+            get: function () {
+                return this._link !== undefined ? this._link : null;
             },
-            offset: {
-              /**
-               * Set/get offset of the text along the path
-               * @property offset
-               * @type {Number}
-               * @default ''
-               * @description Set/get offset of the text along the path
-               * @example
-               * <caption>Set the offset of the text along the path</caption>
-               * // Set the offset to 10
-               * textPath.offset(10);
-               */
-              get: function () {
-                return this._offset !== undefined ? this._offset : '';
-              },
-              set: function (value) {
-                if (value !== undefined && value !== null && value === value) { // last check is for NaN
-                  this._offset = value;
-                  this._textPath.setAttribute("startOffset", value);
-                  return true;
+            set: function (value) {
+                if (value !== undefined && this._link !== value && value !== null) {
+                    this._link = value;
+                    this._textPath.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#link_" + this._link.id());
+                    this._pathElement = this._link.view('path').dom().$dom;
+                    return true;
                 } else {
-                  return false;
+                    return false;
                 }
-              }
+            }
+          },
+          offset: {
+            /**
+             * Set/get offset of the text along the path
+             * @property offset
+             * @type {Number}
+             * @default ''
+             * @description Set/get offset of the text along the path
+             * @example
+             * <caption>Set the offset of the text along the path</caption>
+             * // Set the offset to 10
+             * textPath.offset(10);
+             */
+            get: function () {
+              return this._offset !== undefined ? this._offset : '';
             },
-            gap: {
-              /**
-               * Set/get gap between the text and the path
-               * @property gap
-               * @type {Number}
-               * @default ''
-               * @description Set/get gap between the text and the path
-               * @example
-               * <caption>Set the gap between the text and the path</caption>
-               * // Set the gap to 0.25
-               * textPath.gap(0.25);
-               */
-              get: function () {
-                return this._gap !== undefined ? this._gap : '';
-              },
-              set: function (value) {
-                if (value !== undefined && value !== null && value === value) { // last check is for NaN
-                  this._gap = value;
-                  this.view().dom().$dom.setAttribute("dy", value);
-                  return true;
-                } else {
-                  return false;
-                }
+            set: function (value) {
+              if (value !== undefined && value !== null && value === value) { // last check is for NaN
+                this._offset = value;
+                this._textPath.setAttribute("startOffset", value);
+                return true;
+              } else {
+                return false;
               }
+            }
+          },
+          gap: {
+            /**
+             * Set/get gap between the text and the path
+             * @property gap
+             * @type {Number}
+             * @default ''
+             * @description Set/get gap between the text and the path
+             * @example
+             * <caption>Set the gap between the text and the path</caption>
+             * // Set the gap to 0.25
+             * textPath.gap(0.25);
+             */
+            get: function () {
+              return this._gap !== undefined ? this._gap : '';
             },
+            set: function (value) {
+              if (value !== undefined && value !== null && value === value) { // last check is for NaN
+                this._gap = value;
+                this.view().dom().$dom.setAttribute("dy", value);
+                return true;
+              } else {
+                return false;
+              }
+            }
+          },
         },
         view: {
-            tag: 'svg:text'
+          tag: 'svg:text'
+        },
+        methods: {
+          init: function (args) {
+            this.inherited(args);
+            this._fixedOffset = 25;
+            this._fixedGap = -1;
+            this._text = this.view().dom();
+            this._textPath = document.createElementNS("http://www.w3.org/2000/svg", "textPath");
+            this.side('source');
+            this.gap(this._fixedGap);
           },
-          methods: {
-            init: function (args) {
-              this.inherited(args);
-              this._fixedOffset = 25;
-              this._fixedGap = -1;
-              this._text = this.view().dom();
-              this._textPath = document.createElementNS("http://www.w3.org/2000/svg", "textPath");
-              this.side('source');
-              this.gap(this._fixedGap);
-            },
-            setModel: function (model) {
-              this.inherited(model);
-            },
-            update: function() {
-              this.inherited();
-              var stageScale = 1;
-              if (this._link !== undefined && this._link !== null) {
-                stageScale = this.link().stageScale();
-              }
-              var offset = this._fixedOffset;
-              if (stageScale !== undefined && stageScale !== null) {
-                offset = offset * stageScale;
-              }
-              var gap = this._fixedGap * stageScale;
-              if (this._side == 'source') {
-                this.offset(offset);
-              } else {
-                var pathLength = this._pathElement.getTotalLength();
-                this.offset(pathLength - offset);
-              }
-              this.gap(gap);
+          setModel: function (model) {
+            this.inherited(model);
+          },
+          update: function() {
+            this.inherited();
+            var stageScale = 1;
+            if (this._link !== undefined && this._link !== null) {
+              stageScale = this.link().stageScale();
             }
+            var offset = this._fixedOffset;
+            if (stageScale !== undefined && stageScale !== null) {
+              offset = offset * stageScale;
+            }
+            var gap = this._fixedGap * stageScale;
+            if (this._side == 'source') {
+              this.offset(offset);
+            } else {
+              var pathLength = this._pathElement.getTotalLength();
+              this.offset(pathLength - offset);
+            }
+            this.gap(gap);
           }
+        }
       });
 
 
